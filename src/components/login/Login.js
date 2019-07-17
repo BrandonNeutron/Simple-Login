3c7ef547640c5cdd90d9256b9ed8562b12625669
Brandon Hahn <blh16b@my.fsu.edu>
2019-07-17 15:55:20 -0400

    Added submit div + styling

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -21,13 +21,20 @@ export default class Login extends Component{
                         </label>
                         <br></br>
                         <label class="field a-field a-field_a2 page__field">
-                            <input class="field__input a-field__input" placeholder="e.g. Melnikov" required></input>
+                            <input type="password" class="field__input a-field__input" placeholder="e.g. Melnikov" required></input>
                             <span class="a-field__label-wrap">
                                 <span class="a-field__label">Password</span>
                             </span>
                         </label>
+
+
                         
                     </div>
+
+                    <div className="submit-button">
+                        Submit
+                    </div>
+
                 </div>
                 
             </div>
